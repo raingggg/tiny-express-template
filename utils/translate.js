d5732f15008f3910c0fedb090bc7199b660ac414
raingggg <raingggg@nomail.com>
2021-11-26 19:49:25 +0800

    translate with good quality

--- utils/translate.js
+++ utils/translate.js
@@ -13,27 +13,44 @@ const translateAll = async (startLocale) => {
   const enFile = require(path.resolve(localePath, en));
   const keys = Object.keys(enFile).sort();
 
-  for (let i = 0; i < files.length; i++) {
+  const total = files.length;
+  for (let i = 0; i < total; i++) {
     const objContent = {};
     const fileName = files[i];
     const locale = fileName.replace('.json', '');
     if (startLocale && locale < startLocale) continue;
 
-    console.log('\nlocale', locale);
+    console.log('\nlocale ', `${locale} - ${i} of ${total}`);
     for (let j = 0; j < keys.length; j++) {
+      if (j % 20 === 0) console.log('processing ', j);
+
       const key = keys[j];
       const val = enFile[key];
       if (fileName === en) {
         objContent[key] = val;
       } else {
-        const res = await translate(val, { client: "dict-chrome-ex", from: 'en', to: locale });
-        objContent[key] = res.data.sentences.map(s => s.trans).join('');
-        console.log(key, [val, objContent[key]]);
+        const res = await retryTrans(val, { client: "dict-chrome-ex", from: 'en', to: locale });
+        objContent[key] = res;
       }
     }
 
     await writeFile(path.resolve(localePath, fileName), JSON.stringify(objContent, null, 2));
   }
+};
+
+const retryTrans = async (val, options) => {
+  let res = '';
+  for (let i = 0; i < 3; i++) {
+    if (res) break;
+
+    try {
+      res = await translate(val, options);
+    } catch (e) {
+      console.log('retrying...');
+    }
+  }
+
+  return res.data.sentences.map(s => s.trans).join('');
 };
 
 translateAll();
